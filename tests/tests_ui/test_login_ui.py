d1b7354c0143b5f092b81044333cda64b1787a2b
Timur Shkabrov <tshkabrov@gmail.com>
2026-03-10 15:24:41 +0500

    add positive login test

--- tests/tests_ui/test_login_ui.py
+++ tests/tests_ui/test_login_ui.py
@@ -2,15 +2,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#postitive test of user registration
-def test_registration(browser, ui_base_url, wait):
-    browser.get(ui_base_url + "/register")
+#positive test for user login
+def test_login(browser, ui_base_url, wait):
+    browser.get(ui_base_url + '/login')
 
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[formcontrolname='username']"))).send_keys("conduit_test")
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[formcontrolname='email']"))).send_keys("conduit_test@test.com")
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[formcontrolname='password']"))).send_keys("conduit_test")
-    browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[type='submit']"))).click()
 
     element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[href*='/profile/']")))
     profile_name = element.text.strip()
     assert profile_name == "conduit_test"
+
