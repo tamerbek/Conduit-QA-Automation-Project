a6951c0c23c9370cfe5dba3b8862ea941787ec54
Timur Shkabrov <tshkabrov@gmail.com>
2026-03-16 18:26:42 +0500

    add registration empty fields api test

--- tests/tests_api/test_registration.py
+++ tests/tests_api/test_registration.py
@@ -17,6 +17,7 @@ def test_user_positive_registration_api(api_base_url):
 
     response = requests.post(api_base_url + "/users", json=payload)
 
+    print(response.text)
     assert response.status_code == 201
     assert "token" in response.json()["user"]
     assert response.json()["user"]["token"] is not None
@@ -67,3 +68,52 @@ def test_user_registration_empty_email(api_base_url):
 
     assert response.status_code == 422
     assert "can't be blank" in response.json()["errors"]["email"]
+
+
+@pytest.mark.api
+@pytest.mark.regression
+def test_user_registration_all_fields_empty(api_base_url):
+    user_data = {
+        "username": "",
+        "email": "",
+        "password": ""
+    }
+    payload = {"user": user_data}
+
+    response = requests.post(api_base_url + "/users", json=payload)
+
+    assert response.status_code == 422
+    assert "can't be blank" in response.json()["errors"]["username"]
+    assert "can't be blank" in response.json()["errors"]["email"]
+    assert "can't be blank" in response.json()["errors"]["password"]
+
+
+@pytest.mark.api
+@pytest.mark.regression
+def test_user_registration_empty_body(api_base_url):
+    payload = {}
+
+    response = requests.post(api_base_url + "/users", json=payload)
+    assert response.status_code == 422
+    assert "can't be blank" in response.json()["errors"]["username"]
+    assert "can't be blank" in response.json()["errors"]["email"]
+    assert "can't be blank" in response.json()["errors"]["password"]
+
+
+@pytest.mark.api
+@pytest.mark.regression
+def test_user_register_without_user_wrapper(api_base_url):
+    user_data = {
+        "email": fake.email(),
+        "password": fake.password(),
+        "username": fake.name()
+    }
+
+    response = requests.post(api_base_url + "/users", json=user_data)
+
+    assert response.status_code == 422
+    assert "can't be blank" in response.json()["errors"]["username"]
+    assert "can't be blank" in response.json()["errors"]["email"]
+    assert "can't be blank" in response.json()["errors"]["password"]
+
+
